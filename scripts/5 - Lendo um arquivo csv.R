a05b7269e76a98132fe65479dd9a24ba1ab46ae6
Marcos Vital (LEQ-UFAL) <marcosvital@gmail.com>
2025-12-01 09:51:24 -0300

    Add files via upload

---
+++ scripts/5 - Lendo um arquivo csv.R
@@ -0,0 +1,14 @@
+# Lendo um arquivo csv
+# Aprendendo e Ensinando R para Análise de dados
+# Prof Marcos Vital, UFAL
+# Novembro de 2025
+# Conteúdo com licença CC BY 4.0
+# https://marcosvital.github.io/Aprendendo-R/
+
+# Vamos ler o arquivo de dados.
+# Confira se ele está na pasta do projeto!
+
+gatos <- read.csv ("Gatos do Marcos.csv")
+
+# Pedindo um resumo do objeto:
+summary (gatos)
